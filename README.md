ef93ff1c9bf4437efad39dc17d433216a9b6c7d1
jtsiv1 <johnspeaksiv@gmail.com>
2025-04-16 13:43:11 -0500

    add embedding training

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# synonymy-representations-in-world-englishes
+# Synonymy Representations in World Englishes
+
+## 0. Sourcing Data
+
+To access the data, visit the webpage for [Professor Jonathan Dunn's *Corpus of Global Language Use*](https://publicdata.canterbury.ac.nz/Research/Geocorpus/). For this project, we are using version CGLU_v5.2. Please follow these steps:
+
+1. Identify the countries of interest within each region directory.
+2. For each country, navigate to the `/eng` folder.
+3. Download the first `.IN` file you find.
+4. Place all downloaded files directly into a folder called `data` without decompressing or renaming them.
+
+Countries Used in This Study:
+
+| Region | Countries |
+| --- | --- |
+| Inner Circle | United States, Canada, United Kingdom, Ireland, Australia, New Zealand |
+| Outer Circle | India, Singapore, Nigeria, Malaysia |
+| Expanding Circle | China, Russia, Brazil |
+
+## 1. Training World English Embedding Spaces
+
+Once all of the data is in place in the data directory you are able to start training the embedding spaces. Go to the first Jupyter notebook in the `src` folder. It is called `1.Train_Embedding_Models.ipynb`. This file walks you through the process of training the embedding spaces. At the start of the file, make sure to specify the route of your data directory and where you would like your models output if they differ. Run the cells cell by cell and make sure the code has completed and generated all the files into the `models` directory (or another directory you specify) before proceeding.
+
+*Alternatively*, there is also a python file derived from the notebook. For some systems, this may run much faster. It is called `1.Train_Embedding_Models.py` and can be run from the terminal.
---
+++ src/1.Train_Embedding_Models.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### 0. Imports
+# 
+# If this code block is failing, make sure you have all of the necessary packages installed
+
+# In[1]:
+
+
+import os
+from gensim.models import Word2Vec
+from gensim.utils import simple_preprocess
+import pandas as pd
+import gzip
+from multiprocessing import Pool
+import multiprocessing
+import time
+
+
+# ### 1. Set Variables and Paths
+# 
+# `data_dir` should match where you have your data saved and `model_dir` is where your embedding models will be output. The cores variable sets the number of cores dedicated to parallelizing preprocessing. By default, half of your cores will be used. Set these to your preference accordingly.
+
+# In[ ]:
+
+
+data_dir = "../data"
+model_dir = "../models"
+cores = multiprocessing.cpu_count() // 2
+
+
+# ### 2. Identify Files for Model Training
+# 
+# This will find **all** files in the `data_dir` specified. You will run into problems in later steps if there are any other files in the directory that cannot be opened as expected or are named in unexpected ways (data is expected to be named `sample_region.Sample_Country.eng.[clean|1].IN.gz`).
+
+# In[3]:
+
+
+file_paths = [os.path.join(data_dir, file).replace("\\", "/") for file in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, file))]
+
+
+# ### 3. Function to Train and Save Embedding Space
+# 
+# This function does the following:
+# 
+# 1. Opens the file into a Pandas dataframe
+# 2. Extracts the text column and preprocesses each sample using simple_preprocess from Gensim (throwing away the rest of the data)
+# 3. Uses Gensim Word2Vec to train a skip-gram model, iterating over the samples 
+# 4. Saves the model to the given directory
+
+# In[ ]:
+
+
+def preprocess_text(texts):
+    return [simple_preprocess(text) for text in texts]
+
+def create_and_save_embedding_model(file_path, timestamp=None):
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+
+    country = os.path.basename(file_path).split(".")[1] # Extract country name from file name
+    model_path = os.path.join(model_dir, country + ".model")
+    
+    if os.path.exists(model_path):
+        print(f"Model for {country} already exists. Skipping...")
+        return
+    print(f"Processing {country}...")
+
+    with gzip.open(file_path, 'rt', encoding='utf-8', errors='replace') as f:
+        df = pd.read_csv(f, encoding='utf-8', on_bad_lines='skip')
+    if timestamp is not None:
+        elapsed = time.time() - timestamp
+        minutes, seconds = divmod(int(elapsed), 60)
+        print(f"\tLoaded {df.shape[0]} rows of data ({minutes}:{seconds:02d})")
+        timestamp = time.time()
+    
+    df = df['Text'].tolist()[1:2000000]  # limit to 2 million rows
+
+    
+    with Pool(processes=cores) as pool:
+        sentences = pool.map(simple_preprocess, df)
+    if timestamp is not None:
+        elapsed = time.time() - timestamp
+        minutes, seconds = divmod(int(elapsed), 60)
+        print(f"\tPreprocessed data ({minutes}:{seconds:02d})")
+        timestamp = time.time()
+
+    del df
+    
+    model = Word2Vec(sentences, vector_size=100, window=5, min_count=1, sg=1, workers=4, epochs=1)
+    model.save(model_path)
+    if timestamp is not None:
+        elapsed = time.time() - timestamp
+        minutes, seconds = divmod(int(elapsed), 60)
+        print(f"\tWord2Vec model saved ({minutes}:{seconds:02d})")
+
+
+# ### 4. Loop Through and Train All Models
+# 
+# Runs the embedding model code for each of the identified files
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    for file_path in file_paths:
+        create_and_save_embedding_model(file_path, timestamp=time.time())
+
